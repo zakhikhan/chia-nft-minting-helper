4ca9d480b9d514946589bcf3acf28cb0a453272d
zakhikhan <76130723+zakhikhan@users.noreply.github.com>
2022-06-30 20:58:45 -0400

    update upload.py

--- upload.py
+++ upload.py
@@ -1,5 +1,5 @@
 import requests
-headers = {'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDc2YjQ1ZTEzMjJmNzFCYzFDYWVkYUY4'}, \
+headers = {'Authorization': 'Bearer API_KEY_HERE'}, \
 {'Content-Type': 'multipart/form-data'}
 payload = {'file' : open('/home/zack/smile/photos/smile3.jpg','rb')}
 r = requests.post("https://api.nft.storage", files=payload, headers=headers)
